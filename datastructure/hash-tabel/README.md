7cce31cd257b9c75efb6933c5832d959dc1d5b0d
Mohammadabufarweh <abufarweh34@gmail.com>
2021-09-05 04:00:06 +0300

    cc30

---
+++ datastructure/hash-tabel/README.md
@@ -0,0 +1,14 @@
+# Challenge 30
+
+**- Implement a Hashtable with the following methods:**
+
+- add: takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+- get: takes in the key and returns the value from the table.
+- contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
+- hash: takes in an arbitrary key and returns an index in the collection.
+
+**- Approach & Efficiency**
+
+- all function take O(1) time.
+
+- This hash table will implement separate chaining as the method to deal with collisions. Separate chaining involves storing entries in the hashtable in an array of linked lists so that even if a collision was to happen, the entry would just be added on to the linked list at that index of the array.
---
+++ datastructure/hash-tabel/src/main/java/HashTable.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class HashTable <T>{
+    Node[] arr;
+    int size;
+    public HashTable(int size){
+        this.size=size;
+        arr=new Node[size];
+        // Define and initilize the arr
+        for (int i=0;i<size;i++){
+            arr[i] = new Node();
+        }
+    }
+     public int hash (int key){
+         return (key*599)%size;
+    }
+    public void add(int key ,Object value){
+        int index = hash(key);
+        Node arrayValue=arr[index];
+        Node newItem=new Node(key,value);
+        newItem.next=arrayValue.next;
+        arrayValue.next=newItem;
+
+    }
+
+    public T get(int key){
+        T value = null;
+        int index=hash(key);
+        Node arrayValue=arr[index];
+
+
+        while (arrayValue!=null){
+            if(arrayValue.getKey()==key){
+                value= (T) arrayValue.getValue();
+                break;
+            }
+            arrayValue=arrayValue.next;
+        }
+        return value;
+    }
+    public boolean contains (int key){
+        int index = hash(key);
+        Node arrayValue=arr[index];
+        while (arrayValue!=null){
+            if (arrayValue.getKey()==key) {
+         return true;
+     }else arrayValue=arrayValue.next;
+}
+        return false;
+    }
+
+    public String toString(){
+        String result = "";
+        if(arr == null){
+            return "hash Table is Empty";
+        }
+        for (int i = 0; i < arr.length; i++) {
+            Node arrayValue = arr[i];
+//            System.out.println(arrayValue.getValue());
+            if(arrayValue!=null){
+
+                    while (arrayValue!=null){
+                        result = result+"Key "+arrayValue.key+" Value "+ arrayValue.value + " \n";
+                        arrayValue = arrayValue.next;
+                    }
+            }
+        }
+        return result;
+    }
+
+}
